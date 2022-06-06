fba64e7cb4462992ec80dac2e8d07325d5261ca1
Ma Pengfei <45191163+PengfeiM@users.noreply.github.com>
2022-06-06 22:20:28 +0800

    Update camera_detection.py

--- camera_detection.py
+++ camera_detection.py
@@ -1,3 +1,6 @@
+'''
+本项目是我在github（国内的话是gitee）的免费开源项目。如果你在某些平台（CSDN、淘宝）付费下载了该项目，烦请告知（邮箱(PengfeiM@outlook.com)）。
+'''
 from torch.autograd import Variable
 from detection import *
 from ssd_net_vgg import *
