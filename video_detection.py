ababab7233cc29483490218cbac0b52c196dae19
Ma Pengfei <45191163+PengfeiM@users.noreply.github.com>
2022-06-06 22:20:56 +0800

    Update video_detection.py

--- video_detection.py
+++ video_detection.py
@@ -3,6 +3,11 @@
 Created on Fri Apr 26 15:58:49 2019
 
 @author: 朋飞
+
+
+本项目是我在github（国内的话是gitee）的免费开源项目。如果你在某些平台（CSDN、淘宝）付费下载了该项目，烦请告知（邮箱(PengfeiM@outlook.com)）。
+
+
 """
 
 from torch.autograd import Variable
