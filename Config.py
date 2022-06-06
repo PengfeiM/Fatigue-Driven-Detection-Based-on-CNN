e19550fcd76b32be1ec07d470d96e411f8073823
Ma Pengfei <45191163+PengfeiM@users.noreply.github.com>
2022-06-06 22:19:41 +0800

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,3 +1,6 @@
+'''
+本项目是我在github（国内的话是gitee）的免费开源项目。如果你在某些平台（CSDN、淘宝）付费下载了该项目，烦请告知（邮箱(PengfeiM@outlook.com)）。
+'''
 import os.path as osp
 sk = [ 15, 30, 60, 111, 162, 213, 264 ]
 feature_map = [ 38, 19, 10, 5, 3, 1 ]
